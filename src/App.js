7a1e35944fbf886d8b8767790bbbc2277398ff82
Rahmanyaz <rahmanyazwork@gmail.com>
2026-03-17 17:42:32 +0200

    fixed issues acording to comment no 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import Project from './projectrepo/project.js';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [pages, setPages] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPages = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/pages");
+        const data = await response.json();
+        setPages(data);
+        console.log("DATA FROM /pages:", data);
+
+      } catch (error) {
+        console.error("Error fetching pages:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPages();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+
   return (
-    <div id ='app'>
+    <div id='app'>
       <div id='header'> 
         <div id='headernexcode'>
-          <p>about us</p>
-          <p>projects</p>
-          <p>members</p>
-          <p>contact</p>
+          {pages.map((page) => (
+            <button id='pages' key={page.id}   onClick={() => {/* Handle button click */}}>
+              {page.title}
+            </button>
+          ))}
         </div>
       </div>
+
       <div id='body'>
         <h1 id='name'>NexCode IT Community</h1>
         <p id='projectsWord'>Projects</p>
-        <Project />
-        <Project />
+
+       <Project/>
+       
+        
       </div>
     </div>
-    
   );
 }
 
--- src/projectrepo/project.js
+++ src/projectrepo/project.js
@@ -1,15 +1,38 @@
 import './project.css';
+import { useEffect, useState } from 'react';
+
 function Project() {
+    const [projects, setProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(()=> {
+        const fetchProjects = async () => {
+            try {
+                const response = await fetch("http://localhost:3001/projects");
+                const data = await response.json();
+                setProjects(data);
+                setLoading(false);
+                console.log("PROJECTS:", data);
+            } catch (error) {
+                console.error("Error fetching projects:", error);
+                setLoading(false);
+            }
+        };
+        fetchProjects();
+    }, []);
+    if (loading) return <p>Loading...</p>;
     return (
-        <div id='project'>
-            <img alt="Photo should come here"></img>
-            <div id='projectInfo'>
-                <p id='title'>Title</p>
-                <p id='description'>Description</p>
-                <p>Description</p>
+        projects.map((project) => (
+            <div id='project'>
+                <div id='projectPhoto'>
+                    <img src={project.photo} alt={project.title} />
+                </div>
+                <div id='projectInfo'>
+                    <p id='title'>{project.title}</p>
+                    <p id='de'>{project.description}</p>
+                </div>
+
             </div>
-           
-        </div>
+        ),[])
     );
  
 }
