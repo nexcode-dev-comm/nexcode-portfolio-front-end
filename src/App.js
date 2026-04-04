f5788c54573d79b5be567c0efa8123bb779921b8
Rahmanyaz <rahmanyazwork@gmail.com>
2026-04-04 20:03:41 +0300

    Local variable is created for Fetchiing errors

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import OurStory from './repostories/ourStory';
 import partHeader from './repostories/partHeader';
 import './App.css';
-import { useEffect, useState } from 'react';
 import PartHeader from './repostories/partHeader';
 =======
 import './App.css';
@@ -10,47 +9,20 @@ import FeaturedProjects from "./projects/FeaturedProjects";
 >>>>>>> 78db1f7 (projects part)
 
 function App() {
-  const [pages, setPages] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchPages = async () => {
-      try {
-        const response = await fetch("http://localhost:3001/pages");
-        const data = await response.json();
-        setPages(data);
-        console.log("DATA FROM /pages:", data);
-
-      } catch (error) {
-        console.error("Error fetching pages:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchPages();
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
+  
 
   return (
 <<<<<<< HEAD
     <div className='app'>
-      <div id='header'> 
-        <div className='headernexcode'>
-          {pages.map((page) => (
-            <button id='pagesButton' key={page.id}   onClick={() => {/* Handle button click */}}>
-              {page.title}
-            </button>
-          ))}
-        </div>
-      </div>
 
       <div className='body'>
         <div className='title'>
           <h1 id='nameCommunity'>NEXCODE</h1>
           <p id='devcommunity' > DEV COMMUNITY</p>
-          <p id="quote">// build. ship. repeat. _</p>
+          <div className='quoteContainer'>
+            <p id="quote">// build. ship. repeat. </p>
+            <p id="quoteCursor">_</p>
+          </div>
         </div>
         <PartHeader contend="OUR STORY" />
         <OurStory />
